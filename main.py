24c7d374c03acb324906423e602e060f32cf2b15
Manan Mehra <mananmehra330@gmail.com>
2023-08-10 17:38:46 +0530

    video feature added

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask, render_template, request, redirect, url_for
 import os
-from flask_mail import Mail,Message
+from flask_mail import Mail, Message
 import json
+import time
+import cv2  # OpenCV for video recording
 
-with open("config.json","r") as c:
-   params=json.load(c)["params"]
+app = Flask(__name__)
 
-app=Flask(__name__)
+# Load configuration parameters from config.json
+with open("config.json", "r") as c:
+    params = json.load(c)["params"]
 
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# Configure Flask app for email
 app.config.update(
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT='465',
@@ -17,7 +19,34 @@ app.config.update(
     MAIL_USERNAME=params['gmail-user'],
     MAIL_PASSWORD=params['gmail-password'],
 )
-mail=Mail(app)
+mail = Mail(app)
+
+# Configure Flask app for video upload
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Set the duration of the interview recording in seconds (60 seconds = 1 minute)
+interview_duration = 60
+
+# Function to record a video for the specified duration
+def record_video(duration):
+    cap = cv2.VideoCapture(0)  # Open the default camera (0)
+    codec = cv2.VideoWriter_fourcc(*'XVID')
+    output_file = 'interview_video.avi'
+    out = cv2.VideoWriter(output_file, codec, 30, (640, 480))
+
+    start_time = time.time()
+    while time.time() - start_time < duration:
+        ret, frame = cap.read()
+        if ret:
+            out.write(frame)
+        else:
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+    return output_file
 
 @app.route('/')
 def home():
@@ -37,10 +66,14 @@ def upload_file():
         filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filename)
         
-        subject = "CV Upload Notification"
+        # Record the video
+        recorded_video = record_video(interview_duration)
+        
+        # Send email notification
+        subject = "CV and Interview Video Upload Notification"
         sender_email = params['gmail-user']
         recipient_email = "example@gmail.com"  # Replace with the recipient's email
-        message_body = "Your CV has been uploaded successfully."
+        message_body = "Your CV and interview video have been uploaded successfully."
         
         msg = Message(subject=subject,
                       sender=sender_email,
@@ -53,8 +86,6 @@ def upload_file():
 @app.route('/job')
 def job():
     return render_template('job.html')
-        
-
 
 if __name__ == '__main__':
     app.run(debug=True)
